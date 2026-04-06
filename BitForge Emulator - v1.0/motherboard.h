78812e76319b74bd398c06245188b95b5d8efa94
Miroslav Pazdera <cryonixdeveloper@gmail.com>
2026-04-06 04:39:13 +0200

    Add files via upload

--- BitForge Emulator - v1.0/motherboard.h
+++ BitForge Emulator - v1.0/motherboard.h
@@ -14,12 +14,15 @@ public:
     static constexpr size_t RAM_SIZE = 128ull * 1024 * 1024;
     static constexpr size_t ROM_SIZE = 32ull * 1024;
     static constexpr size_t STACK_SIZE = 1024ull * 1024;
+    static constexpr size_t IVT_SIZE = 2048ull;
 
     static constexpr uint64_t STACK_END = 0x00000000;
     static constexpr uint64_t STACK_START = STACK_END + STACK_SIZE - 1;
     static constexpr uint64_t ROM_START = STACK_START + 1;
     static constexpr uint64_t ROM_END   = ROM_START + ROM_SIZE - 1;
-    static constexpr uint64_t RAM_START = ROM_END + 1;
+    static constexpr uint64_t IVT_START = ROM_END + 1;
+    static constexpr uint64_t IVT_END = IVT_START + IVT_SIZE - 1;
+    static constexpr uint64_t RAM_START = IVT_END + 1;
     static constexpr uint64_t RAM_END = RAM_START + RAM_SIZE - 1;
 
     Motherboard();
